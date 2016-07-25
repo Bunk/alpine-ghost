1d1cd0c7ce321c84a58c9c9e687790f6105bdd43
JD Courtoy <jd.courtoy@gmail.com>
2016-07-25 10:19:29 -0500

    Prefer the config.js file baked into the image if present

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,19 +12,26 @@ if [[ "$*" == npm*start* ]]; then
         fi
     done
 
-    if [ ! -e "$GHOST_CONTENT/config.js" ]; then
-        sed -r '
-            s/127\.0\.0\.1/0.0.0.0/g;
-            s!path.join\(__dirname, (.)/content!path.join(process.env.GHOST_CONTENT, \1!g;
-        ' "$GHOST_SOURCE/config.example.js" > "$GHOST_CONTENT/config.js"
-    fi
-
+    # Prefer a configuration to be built into the image under
     if [ ! -e "$GHOST_SOURCE/config.js" ]; then
+        # There isn't a hard-wired configuration
+        # Let's use the one supplied as an override
+
+        if [ ! -e "$GHOST_CONTENT/config.js" ]; then
+            # There isn't a supplied override.
+            # Let's convert the example, replacing for use with env vars
+            sed -r '
+                s/127\.0\.0\.1/0.0.0.0/g;
+                s!path.join\(__dirname, (.)/content!path.join(process.env.GHOST_CONTENT, \1!g;
+            ' "$GHOST_SOURCE/config.example.js" > "$GHOST_CONTENT/config.js"
+        fi
+
         ln -sf "$GHOST_CONTENT/config.js" "$GHOST_SOURCE/config.js"
     fi
 
+    # Supply ownership of the user content directory
     chown -R app "$GHOST_CONTENT"
-    # Required for production mode
+    # Supply ownership of the source content for production-mode
     chown -R app "$GHOST_SOURCE/content"
 
     set -- su-exec app "$@"
