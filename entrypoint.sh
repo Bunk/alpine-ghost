8c45a4f3556e60deefa854ee5c0420fc1f20bf59
JD Courtoy <jd.courtoy@gmail.com>
2016-07-30 16:44:07 -0500

    Revert "Prefer the config.js file baked into the image if present"
    This reverts commit 1d1cd0c7ce321c84a58c9c9e687790f6105bdd43.

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,26 +12,17 @@ if [[ "$*" == npm*start* ]]; then
         fi
     done
 
-    # Prefer a configuration to be built into the image under
-    if [ ! -e "$GHOST_SOURCE/config.js" ]; then
-        # There isn't a hard-wired configuration
-        # Let's use the one supplied as an override
-
-        if [ ! -e "$GHOST_CONTENT/config.js" ]; then
-            # There isn't a supplied override.
-            # Let's convert the example, replacing for use with env vars
-            sed -r '
-                s/127\.0\.0\.1/0.0.0.0/g;
-                s!path.join\(__dirname, (.)/content!path.join(process.env.GHOST_CONTENT, \1!g;
-            ' "$GHOST_SOURCE/config.example.js" > "$GHOST_CONTENT/config.js"
-        fi
-
-        ln -sf "$GHOST_CONTENT/config.js" "$GHOST_SOURCE/config.js"
+    if [ ! -e "$GHOST_CONTENT/config.js" ]; then
+        sed -r '
+            s/127\.0\.0\.1/0.0.0.0/g;
+            s!path.join\(__dirname, (.)/content!path.join(process.env.GHOST_CONTENT, \1!g;
+        ' "$GHOST_SOURCE/config.example.js" > "$GHOST_CONTENT/config.js"
     fi
 
-    # Supply ownership of the user content directory
+    ln -sf "$GHOST_CONTENT/config.js" "$GHOST_SOURCE/config.js"
+
     chown -R app "$GHOST_CONTENT"
-    # Supply ownership of the source content for production-mode
+    # Required for production mode
     chown -R app "$GHOST_SOURCE/content"
 
     set -- su-exec app "$@"
